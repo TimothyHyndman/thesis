bd77f112747034abee3f3338cafadccf515f2bca
Unknown <timothy.hyndman@gmail.com>
2018-11-05 15:48:26 +1100

    Edit flag graph codes

--- Figures/Mixtures/Code/create_normal_flag_graph.m
+++ Figures/Mixtures/Code/create_normal_flag_graph.m
@@ -14,13 +14,13 @@ phi = @(x,u) (1/(sigma*sqrt(2*pi)))*exp(-((repmat(x,[1,length(u)]) - repmat(u,[l
 % phi = @(x,u) (1/(pi*lambda))*(1 + ((repmat(x,[1,length(u)]) - repmat(u,[length(x),1]))./lambda).^2).^(-1);
 
 
-res = 32;   %ultra-1024, vhigh-512, high-256
+res = 1024;   %ultra-1024, vhigh-512, high-256
 width = 5;
 
 Z = calculate_flag_graph(phi, width, res);
 
 
 options.line_width = 1;
-options.save = false;
-options.filename = 'blahblah';
+options.save = true;
+options.filename = '';
 plot_flag_graph(Z, sigma, width, options);
