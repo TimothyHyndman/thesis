648b3bef7529c779cdb48ada2b90cc598234dfec
Unknown <timothy.hyndman@gmail.com>
2018-11-05 15:02:02 +1100

    Add flag graph code

---
+++ Figures/Mixtures/Code/MixtureLikelihoodMovingMasses2.m
@@ -0,0 +1,232 @@
+function [Q,log_likelihood] = MixtureLikelihoodMovingMasses2(phi,X)
+% Mixing Homogeneity Paper Log Likelihood Stuff
+
+%Example usage: 
+% sigma = 0.5;
+% phi = @(x,u) (1/(sigma*sqrt(2*pi)))*exp(-((repmat(x,[1,length(u)]) - repmat(u,[length(x),1])).^2)./(2*sigma^2));
+% Y = unifrnd(0,1,[50,1]);
+% [Q, log_likelihood] = MixtureLikelihoodMovingMasses(phi,Y);
+
+m = min(10,length(X));
+a = min(X);
+b = max(X);
+
+Q.Support = linspace(a,b,m);
+
+if m == 1
+    Q.Support = mean(X);
+elseif length(X) <= 3
+    Q.Support = X';
+    m = length(X);
+end
+
+Q.ProbWeights = (1/m)*ones(1,m);  
+
+x0 = [Q.Support,Q.ProbWeights]; 
+func = @(x) -L(phi,X,[x,1 - sum(x((end+1)/2 + 1:end))]);
+x0(end) = [];
+log_likelihood = -func(x0);
+
+%Make sure that initial point is defined
+if func(x0) == Inf
+    display('Initial point undefined')
+    looping = true;
+    while looping
+        Q.Support = unifrnd(a,b,[1,m]);
+        x0 = [Q.Support,Q.ProbWeights];
+        x0(end) = [];
+        display(x0)
+        if func(x0) < Inf
+            looping = false;
+            log_likelihood = -func(x0);
+        end
+    end
+end
+
+options = optimoptions('fmincon','Algorithm','sqp','Display','off','TolFun',1.0e-09,'TolX',1.0e-09);  %sqp algorithm doesn't get stuck on some problems that default does
+tolerance = 1.0e-06;   %Any lower and it gets stuck
+counter = 1;
+looping = true;
+
+while looping
+    %Summation and boundary constraints
+    m = (length(x0)+1)/2;
+    lb_points = a*ones([1,m]) - 0.01;
+    ub_points = b*ones([1,m]) + 0.01;
+    A_points = zeros([1,m]);
+    
+    lb_masses = zeros([1,m-1]);
+    ub_masses = ones([1,m-1]);
+    A_masses = ones([1,m-1]);
+
+    lb = [lb_points,lb_masses];
+    ub = [ub_points,ub_masses];
+    A = [A_points,A_masses];
+    b = 1;
+
+    %Solve
+    [solution_x,fval] = fmincon(func,x0,A,b,[],[],lb,ub,[],options);
+    if -fval >= log_likelihood
+        log_likelihood = -fval;
+        Q.Support = solution_x(1:m);
+        Q.ProbWeights = [solution_x(m+1:end),1 - sum(solution_x(m+1:end))];
+        Q = SimplifyDist(Q);
+    else
+%         display(['fmincon ',num2str(counter),' failed'])
+    end
+
+    [theta_star,derivative] = max_theta2(phi,X,Q);
+
+    if derivative <= 0 + tolerance
+        break
+    else
+        [Q_test,log_likelihood_test] = max_weights(phi,X,Q,theta_star);
+        Q_test = SimplifyDist(Q_test);
+        if log_likelihood_test >= log_likelihood
+            Q = Q_test;
+            log_likelihood = log_likelihood_test;
+            x0 = [Q.Support,Q.ProbWeights];
+            x0(end) = [];
+            Q = SimplifyDist(Q);
+            [~,derivative] = max_theta2(phi,X,Q);
+            if derivative <= 0+ tolerance
+                break
+            end
+        else
+%             display(['max weights ',num2str(counter),' failed'])
+        end
+    end
+
+%Used for testing points that won't converge
+    counter = counter+1;
+    if counter > 10
+%         display(X)
+        display('FAILED')  
+%         [theta_star,derivative] = max_theta2(phi,X,Q);
+%         display([theta_star,derivative])
+        break
+    end
+end
+
+% plotDD(phi, X, Q)
+end
+
+%Subfunctions
+function log_likelihood = L(phi,X,x)
+Support = x(1:length(x)/2);
+ProbWeights = x(length(x)/2+1:end);
+log_likelihood = sum(log(ProbWeights*phi(X,Support)'));
+end
+
+function derivative = D(phi,X,theta,Q)
+f_Q = Q.ProbWeights*phi(X,Q.Support)';
+f_theta = phi(X,theta)';
+denom = repmat(f_Q,[length(theta),1]);
+derivative = sum(f_theta./denom - 1,2);
+end
+
+function [theta_star,derivative] = max_theta(phi,X,Q)
+DQ = @(theta) D(phi,X,theta,Q);
+res = 100000; %Need this high res near solution
+xx = linspace(min(X),max(X),res);
+yy = DQ(xx);
+[derivative,ii] = max(yy);
+theta_star = xx(ii);
+end
+
+function [theta_star,derivative] = max_theta2(phi,X,Q)
+DQ = @(theta) D(phi,X,theta,Q);
+res = 1000;
+% As 
+xx = linspace(min(X),max(X),res);
+dx = xx(2) - xx(1);
+yy = DQ(xx);
+[~,ii] = max(yy);
+%Fine
+res2 = 1000;
+xx = linspace(xx(ii) - dx,xx(ii)+dx,res2);
+yy = DQ(xx);
+[derivative,ii] = max(yy);
+theta_star = xx(ii);
+end
+
+function [Q,log_likelihood] = max_weights(phi,X,Q,theta_star)
+func = @(weights) -L(phi,X,[Q.Support,theta_star,weights,1 - sum(weights)]);   %function to minimize
+lb = zeros([1,length(Q.Support)]);
+A = ones([1,length(Q.Support)]);
+b = 1;
+options = optimoptions('fmincon','Algorithm','sqp','Display','off','TolFun',1.0e-09,'TolX',1.0e-09);
+x0 = Q.ProbWeights;
+% lh1 = -func(x0)
+[probweights,fval] = fmincon(func,x0,A,b,[],[],lb,[],[],options);
+% 1 - sum(probweights)
+% lh2 = -func(probweights)
+log_likelihood = -fval;
+Q.Support = [Q.Support,theta_star];
+Q.ProbWeights = [probweights,1 - sum(probweights)];
+end
+
+function plotDD(phi,X,Q)
+DQ = @(theta) D(phi,X,theta,Q);
+res = 1000;
+xx = linspace(min(X),max(X),res);
+yy = DQ(xx);
+plot(xx,yy)
+end
+
+function Q = SimplifyDist(Q)
+grid_points = Q.Support;
+grid_masses = Q.ProbWeights;
+zero_tolerance = 0.0000005;  %Need this at least as small as 0.0000005
+search_size = 0.05;  %Getting points close to each other is rare - unlike getting points with low mass.
+[grid_points,I] = sort(grid_points);
+grid_masses = grid_masses(I);
+
+%Get rid of zeros
+index = 1;
+looping = true;
+while looping
+    if grid_masses(index) < zero_tolerance
+        grid_points(index:end-1) = grid_points(index+1:end);
+        grid_points(end) = [];
+        grid_masses(index:end-1) = grid_masses(index+1:end);
+        grid_masses(end) = [];
+    else
+        index = index+1;
+    end
+    if index >= length(grid_points)
+        looping = false;
+    end
+end
+
+if grid_masses(end) < zero_tolerance
+    grid_masses(end) = [];
+    grid_points(end) = [];
+end
+
+%Get rid of duplicates
+index = 1;
+looping = true;
+if length(grid_points) < 2
+    looping = false;
+end
+while looping
+    if abs(grid_points(index) - grid_points(index+1)) < search_size
+        grid_points(index) = (grid_points(index)*grid_masses(index)+grid_points(index+1)*grid_masses(index+1))/(grid_masses(index)+grid_masses(index+1));  %Weighted average location
+        grid_points(index+1:end-1) = grid_points(index+2:end);
+        grid_masses(index) = grid_masses(index)+grid_masses(index+1);
+        grid_masses(index+1:end-1) = grid_masses(index+2:end);
+        grid_masses(end) = [];
+        grid_points(end) = [];
+    else
+        index = index+1;
+    end
+    if index == length(grid_points)
+        looping = false;
+    end
+end
+
+grid_masses = grid_masses/sum(grid_masses);    %Normalise
+Q.Support = grid_points;
+Q.ProbWeights = grid_masses;
+end
---
+++ Figures/Mixtures/Code/plot_flag_graph.m
@@ -0,0 +1,152 @@
+%Better looking graph
+function h = plot_flag_graph(Z, sigma, width, options)
+
+options = fill_default_options(options);
+
+res = size(Z,1)-1;
+y1 = linspace(-width,width,res);
+y2 = linspace(-width,width,res);
+y1 = [y1,y1(res)+y1(res) - y1(res-1)];  %*awful hack so we display final row and column of data in plot
+y2 = [y2,y2(res)+y2(res) - y2(res-1)];  %*
+[Y1,Y2] = meshgrid(y1,y2);
+
+h = pcolor(Y1,Y2,Z);
+shading flat
+axis equal tight
+% cmap = gray(3);
+% cmap = parula(3);
+% cmap = summer(3);
+% cmap = [0.3 0.3 0.3;0.2 0.4 0.3;0.3 0.3 0.3];
+cmap = [255 189 76; 180 114 0;103 65 0]/255;    %Light to Dark Highlight
+cmap = [103 65 0;180 114 0;255 189 76]/255;    %Dark to Light Highlight
+cmap = [0 51 104;38 82 127;76 164 255]/255; %Darkt to Light Primary
+% cmap = [76 164 255;38 82 127;0 51 104]/255; %Light to Dark Primary
+colormap(cmap);
+hcb = colorbar;
+
+% set(hcb,'YTick',[1.333,2,2.667],'YTickLabel',[1,2,3],'Location','EastOutside','position',[0.73 .11 .05 .3],'FontSize',20)
+set(hcb,'YTick',[1.333,2,2.667],'YTickLabel',[1,2,3],'Location','EastOutside','FontSize',20)
+
+% Labels
+xlabel('x_2','FontSize',20)
+ylabel('x_3','FontSize',20)
+% title('Number of probability masses required (x_1 = 0)','FontSize',20)
+
+mid = 0 + width/(res-1);
+
+%Axis lines
+if options.axis_lines
+    line([mid mid], [min(y1) max(y1)],'Color','k');  %y-axis
+    line([min(y2) max(y2)], [mid mid],'Color','k');  %x-axis
+end
+
+% Ellipse
+if options.ellipse
+    colour = 'r';
+    colour = [255 189 76]/255;
+%     options.line_width = 2;
+    
+    t = linspace(0,2*pi,100);
+    b = sqrt(3)*sigma;
+    a = sqrt(3)*b; %What we would expect if this ellipse was the intersection
+    %of the plane with a cyclinder along the axis x = y = z
+    ellipse_x = (a*cos(t) - b*sin(t))/sqrt(2)+mid;
+    ellipse_y = (a*cos(t) + b*sin(t))/sqrt(2)+mid;
+    hold on
+    plot(ellipse_x,ellipse_y,'Color',colour,'LineWidth',options.line_width)
+    hold off
+end
+
+%Distance 2*sigma lines
+if options.sigma_lines
+%     colour = 'k';
+%     colour = 'w';
+    colour = [0.8,0.8,0.8];
+%     options.line_width = 2;
+    
+%     dist = 2*sigma;
+        dist = 2*sigma;   %For normal
+%     dist = 2*sqrt(1/3)*sigma;   %For Cauchy
+    
+    hold on
+    
+    line([mid+dist mid+dist], [min(y1) max(y1)],'Color',colour,'LineWidth',options.line_width);  
+    line([mid-dist mid-dist], [min(y1) max(y1)],'Color',colour,'LineWidth',options.line_width);  
+
+    line([min(y1) max(y1)],[mid-dist mid-dist],'Color',colour,'LineWidth',options.line_width); 
+    line([min(y1) max(y1)],[mid+dist mid+dist],'Color',colour,'LineWidth',options.line_width);
+
+    shift = dist;
+    x_line_1 = linspace(min(y1),max(y1)-shift,100);
+    x_line_2 = linspace(min(y1)+shift,max(y1),100);
+    y_line_1 = x_line_1 + shift;
+    y_line_2 = x_line_2 - shift;
+    plot(x_line_1,y_line_1,'Color',colour,'LineWidth',options.line_width)
+    plot(x_line_2,y_line_2,'Color',colour,'LineWidth',options.line_width)
+    hold off
+end
+
+% Linsday1983b bound
+if options.lindsay_bound
+    colour = 'w';
+    colour = [255 217 152]/255;
+    colour = [255 189 76]/255;
+%     options.line_width = 2;
+    
+    dist = 2*sigma;   %For normal
+%     dist = 2*sqrt(1/3)*sigma;   %For Cauchy
+    
+    line([dist+mid,mid],[dist+mid,dist+mid],'Color',colour,'LineWidth',options.line_width);
+    line([dist+mid,dist+mid],[mid,dist+mid],'Color',colour,'LineWidth',options.line_width);
+
+    line([-dist+mid,mid],[-dist+mid,-dist+mid],'Color',colour,'LineWidth',options.line_width);
+    line([-dist+mid,-dist+mid],[mid,-dist+mid],'Color',colour,'LineWidth',options.line_width);
+
+    line([mid,dist+mid],[-dist+mid,mid],'Color',colour,'LineWidth',options.line_width)
+    line([-dist+mid,mid],[mid,dist+mid],'Color',colour,'LineWidth',options.line_width)
+end
+
+if options.save
+    %Print to file... Make sure that the current folder is where you want to
+    %save it!!!
+    if options.filename == []
+        filename = ['Sigma',num2str(sigma),'n3res',num2str(res),'width',num2str(width)];
+        if options.ellipse
+            filename = [filename,'ellipse'];
+        end
+        if options.lindsay_bound
+            filename = [filename,'lindsaybound'];
+        end
+        filename = [filename,'-blue'];
+    else
+        filename = options.filename
+    end
+
+    % export_fig(filename,'-png','-transparent','-r600')
+    export_fig(filename,'-pdf')
+    end
+end
+
+function options = fill_default_options(options)
+    if ~isfield(options, 'ellipse')
+        options.ellipse = false;
+    end
+    if ~isfield(options, 'lindsay_bound')
+        options.lindsay_bound = false;
+    end
+    if ~isfield(options, 'sigma_lines')
+        options.sigma_lines = false;
+    end
+    if ~isfield(options, 'save')
+        options.save = false;
+    end
+    if ~isfield(options, 'axis_lines')
+        options.axis_lines = true;
+    end
+    if ~isfield(options, 'line_width')
+        options.line_width = 1;
+    end
+    if ~isfield(options, 'filename')
+        options.filename = [];
+    end
+end
