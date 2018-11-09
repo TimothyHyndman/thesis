a31b5c621d1af12ba0555a9b400c3616127a5877
Unknown <timothy.hyndman@gmail.com>
2018-11-09 11:15:30 +1100

    Manual optimization for VEM

--- Figures/Mixtures/Code/VEM.m
+++ Figures/Mixtures/Code/VEM.m
@@ -6,13 +6,18 @@ function [Q,log_likelihood] = VEM(phi,X)
 	Q.Support = mean(X);
 	Q.ProbWeights = 1;
 
+	% m = 10;
+	% Q.Support = linspace(min(X), max(X), m);
+	% Q.ProbWeights = (1 / m) * ones(1, m);  
+
 	looping = true;
 
 	while looping
 		% Find theta* = argmax D(theta)
 		[theta_star, derivative] = max_theta2(phi, X, Q);
+		theta_star
 
-		if derivative < 1e-3
+		if derivative < 1e-2
 			looping = false;
 		end
 
@@ -23,38 +28,54 @@ function [Q,log_likelihood] = VEM(phi,X)
 		% Find point of support theta- which minimizes D(theta_j)
 		[~, index_min_support] = min(D(phi, X, Q, Q.Support));
 
-		% Find how much mass to move from theta- to theta*
-		fun = @(x) -blah(phi, X, Q, index_min_support, x);
+		%Find how much mass to move from theta- to theta*
+		res = 100;
+		h_max = Q.ProbWeights(index_min_support);
+		h = linspace(0, h_max, res);
+		delta_h = h(2) - h(1);
+		likelihoods = zeros(1, res);
+		for i = 1:res
+			likelihoods(i) = blah(phi, X, Q, index_min_support, h(i));
+		end
+		[~, index_max] = max(likelihoods);
+		coarse_h_sol = h(index_max)
+
+		% figure(1)
+		% plot(h, likelihoods)
+		% drawnow
+		% pause
 
 		res = 100;
-		xx = linspace(0, Q.ProbWeights(index_min_support), res);
+		
+		h = linspace(max(coarse_h_sol - delta_h, 0), min(coarse_h_sol + delta_h, h_max), res);
+		likelihoods = zeros(1, res);
 		for i = 1:res
-			likelihoods(i) = -fun(xx(i));
+			likelihoods(i) = blah(phi, X, Q, index_min_support, h(i));
 		end
-		figure(1)
-		plot(xx, likelihoods)
-		drawnow
-		delta = fminbnd(fun, 0, Q.ProbWeights(index_min_support))
+		[lll, index_max] = max(likelihoods);
+		h_sol = h(index_max)
 
-		% res = 100;
-		% h = linspace(0, Q.ProbWeights(index_min_support), res);
-		% likelihoods = zeros(1, res);
-		% for i = 1:res
-		% 	Q_test = Q;
-		% 	Q_test.ProbWeights(end) = h(i);
-		% 	Q_test.ProbWeights(index_min_support) = Q_test.ProbWeights(index_min_support) - h(i);
-		% 	likelihoods(i) = L(phi, X, Q_test);
-		% end
-		% [~, index_max] = max(likelihoods);
-		% delta = h(index_max);
+		% figure(1)
+		% plot(h, likelihoods)
+		% drawnow
+		% pause
 
-		Q.ProbWeights(end) = delta;
-		Q.ProbWeights(index_min_support) = Q.ProbWeights(index_min_support) - delta;
+		
+
+		% h_sol = Q.ProbWeights(index_min_support);
+
+
+		Q.ProbWeights(end) = h_sol;
+		Q.ProbWeights(index_min_support) = Q.ProbWeights(index_min_support) - h_sol;
 
 		index_remove = (Q.ProbWeights == 0);
 
 		Q.ProbWeights(index_remove) = [];
 		Q.Support(index_remove) = [];
+		Q.ProbWeights = Q.ProbWeights / sum(Q.ProbWeights);
+
+		Q
+		% lll
 	end
 
 	
@@ -79,6 +100,9 @@ function [theta_star,derivative] = max_theta2(phi, X, Q)
 	yy = DQ(xx);
 	figure(2)
 	plot(xx, yy)
+	hold on
+	scatter(Q.Support, Q.ProbWeights)
+	hold off
 	drawnow
 	[~, ii] = max(yy);
 
