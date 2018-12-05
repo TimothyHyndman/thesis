6f025b7ecd030cc4ce40db6f5498db857690a34d
Unknown <timothy.hyndman@gmail.com>
2018-12-05 13:22:30 +1100

    Add plots to assumption 6 verification

---
+++ Figures/Mixtures/Code/assumption6.R
@@ -0,0 +1,31 @@
+# Plot graphs of cauchy and normal derivatives showing that they satisfy assumption 6 in mixture chapter, 2d result.
+setwd("C:/Users/timot/Git/thesis/Figures/Mixtures/Code")
+library(tidyverse)
+
+# Normal
+x <- seq(-1, 4, length.out = 1000)
+y1 <- -x * exp(-x^2/2)
+y2 <- (x - 2) * exp(-(x - 2)^2/2)
+
+df <- tibble(x = x, y1 = y1, y2 = y2) %>%
+    gather(y1, y2, key = "line", value = "y") %>%
+    mutate(
+        line = str_replace(line, "y1", "f'(x)"),
+        line = str_replace(line, "y2", "-f'(x - 2)"))
+
+
+df %>% ggplot(aes(x, y, colour = line)) + geom_line()
+ggsave("../assumption6_normal.png")
+
+# Cauchy
+y1 <- -2*x / (1 + x^2)^2
+y2 <- 2*(x - 2/sqrt(3)) / (1 + (x - 2/sqrt(3))^2)^2
+
+df <- tibble(x = x, y1 = y1, y2 = y2) %>%
+    gather(y1, y2, key = "line", value = "y") %>%
+    mutate(
+        line = str_replace(line, "y1", "g'(x)"),
+        line = str_replace(line, "y2", "-g'(x - 2)"))
+
+df %>% ggplot(aes(x, y, colour = line)) + geom_line()
+ggsave("../assumption6_cauchy.png")
