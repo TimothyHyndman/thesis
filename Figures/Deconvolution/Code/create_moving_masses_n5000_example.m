5915e072537b8c3dd57819ebddaab734bbc3ccdc
Unknown <timothy.hyndman@gmail.com>
2019-01-03 14:35:42 +1100

    Add n=5000 example

---
+++ Figures/Deconvolution/Code/create_moving_masses_n5000_example.m
@@ -0,0 +1,40 @@
+% Moving masses example
+
+%Initiate random seeds to be able to replicate the results
+rng(5000)
+
+% X
+NSR = 0.2;
+n = 5000;
+dchi = 3;
+X = chi2rnd(dchi,1,n);
+varX = 2 * dchi;
+X = X / sqrt(varX);
+varX = 1;
+varXlong=2*dchi;
+cvar=sqrt(varXlong);
+truedens=@(xx) cvar*chi2pdf(xx*cvar,dchi);
+
+% U
+sigU = sqrt(NSR * varX);
+U = normrnd(0, sigU, 1, n);
+
+% W
+W = X + U;
+h=bwsjpiSM(W');
+
+
+xx_moving = linspace(min(W), max(W), 100);
+
+[yy_moving, Q_moving, tt_moving, optim_values_moving] = moving_mass_deconvolve(W, xx_moving);
+
+options.save = false;
+options.filename = 'moving_masses_n5000_example.png';
+options.plot_histogram = false;
+options.plot_density = true;
+options.plot_masses = true;
+options.true_dens = truedens;
+options.plot_true_dens = true;
+options.naivebw = h;
+options.plot_naive = true;
+plot_deconvolution_graph(Q_moving, xx_moving, yy_moving, W, options);
--- Figures/Deconvolution/Code/moving_mass_deconvolve.m
+++ Figures/Deconvolution/Code/moving_mass_deconvolve.m
@@ -1,7 +1,7 @@
 function [yy, Q, tt, optim_values] = moving_mass_deconvolve(W, xx, m)
    
     if nargin < 3
-        m = 5;
+        m = 10;
     end
    [yy, Q, tt, optim_values] = decon_err_sym(W, xx, m);
    optim_values = orderfields(optim_values);
