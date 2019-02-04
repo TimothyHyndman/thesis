9106d2d8c8349a9d94944a6e49ed175c2ebbd1a2
Unknown <timothy.hyndman@gmail.com>
2019-02-04 11:10:21 +1100

    Add code for R package example

---
+++ Figures/Deconvolution/Code/create_R_example.R
@@ -0,0 +1,39 @@
+
+
+set.seed(5000)
+n <- 500
+NSR <- 0.2
+
+# X
+dchi <- 3
+X <- rchisq(n, df = dchi)
+varX <- 2 * dchi
+X <- X / sqrt(varX)
+varX <- 1
+cvar <- sqrt(2 * dchi)
+
+truedens <- function(xx) {
+	cvar*dchisq(xx * cvar, df = dchi)
+}
+
+# U
+sigU <- sqrt(NSR * varX)
+U <- rnorm(n, mean = 0, sd = sigU)
+
+# W
+W <- X + U
+
+
+decon_object <- deconvolve(W, show_diagnostics = TRUE, m = 10)
+
+xx <- decon_object$x
+yy <- truedens(xx)
+df1 <- data.frame(x = xx, y = yy)
+df2 <- data.frame(x = decon_object$support, y = decon_object$probweights)
+
+truedenscol <- rgb(102, 177, 255, maxColorValue = 255)
+pointcol <- rgb(150, 150, 150, maxColorValue = 255)
+
+plot(decon_object) + 
+	geom_line(data = df1, aes(x, y), colour = truedenscol, size = 1) + 
+	geom_point(data = df2, aes(x, y), colour = pointcol)
