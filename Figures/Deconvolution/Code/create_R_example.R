c63ecf71d14b7a7c6b858f4382e01994bc634ba1
Unknown <timothy.hyndman@gmail.com>
2019-02-05 15:12:12 +1100

    Add discussion to R section, enhance R example

--- Figures/Deconvolution/Code/create_R_example.R
+++ Figures/Deconvolution/Code/create_R_example.R
@@ -3,6 +3,8 @@
 library(deconvolve)
 library(ggplot2)
 library(tictoc)
+library(R.matlab)
+
 
 set.seed(5000)
 n <- 500
@@ -26,6 +28,13 @@ U <- rnorm(n, mean = 0, sd = sigU)
 
 # W
 W <- X + U
+# Same as matlab
+W <- readMat('../thesis/Figures/Deconvolution/Code/Wexample.mat')
+W <- as.vector(W$W)
+
+fWEF <- readMat('../thesis/Figures/Deconvolution/Code/naiveexample.mat')
+fWEF <- as.vector(fWEF$fWEF)
+h <- 0.221444788809846
 
 tic()
 decon_object <- deconvolve(W, show_diagnostics = TRUE, m = 20)
@@ -35,13 +44,16 @@ xx <- decon_object$x
 yy <- truedens(xx)
 df1 <- data.frame(x = xx, y = yy)
 df2 <- data.frame(x = decon_object$support, y = decon_object$probweights)
+df3 <- data.frame(x = xx, y = fWEF)
 
 truedenscol <- rgb(102, 177, 255, maxColorValue = 255)
 pointcol <- rgb(150, 150, 150, maxColorValue = 255)
 
 plot(decon_object) +
+    geom_line(data = df3, aes(x, y), colour = truedenscol, size = 1, linetype = "dashed") +
 	geom_line(data = df1, aes(x, y), colour = truedenscol, size = 1) +
 	geom_point(data = df2, aes(x, y), colour = pointcol)
+
 
 save_image <- FALSE
 
