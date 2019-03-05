38237e558ac3f13ae0243392ef318aac9b8614e7
Unknown <timothy.hyndman@gmail.com>
2019-03-05 13:32:16 +1100

    Continue fixing Peter's comments on mixtures

--- Figures/Mixtures/Code/assumption6.R
+++ Figures/Mixtures/Code/assumption6.R
@@ -1,5 +1,6 @@
 # Plot graphs of cauchy and normal derivatives showing that they satisfy assumption 6 in mixture chapter, 2d result.
-setwd("C:/Users/timot/Git/thesis/Figures/Mixtures/Code")
+#setwd("C:/Users/timot/Git/thesis/Figures/Mixtures/Code")
+setwd("C:/Users/Timothy/Git/thesis/Figures/Mixtures/Code")
 library(tidyverse)
 
 # Normal
@@ -10,8 +11,8 @@ y2 <- (x - 2) * exp(-(x - 2)^2/2)
 df <- tibble(x = x, y1 = y1, y2 = y2) %>%
     gather(y1, y2, key = "line", value = "y") %>%
     mutate(
-        line = str_replace(line, "y1", "f'(x)"),
-        line = str_replace(line, "y2", "-f'(x - 2)"))
+        line = str_replace(line, "y1", "f_norm'(x)"),
+        line = str_replace(line, "y2", "-f_norm'(x - 2)"))
 
 
 df %>% ggplot(aes(x, y, colour = line)) + geom_line()
@@ -24,8 +25,8 @@ y2 <- 2*(x - 2/sqrt(3)) / (1 + (x - 2/sqrt(3))^2)^2
 df <- tibble(x = x, y1 = y1, y2 = y2) %>%
     gather(y1, y2, key = "line", value = "y") %>%
     mutate(
-        line = str_replace(line, "y1", "g'(x)"),
-        line = str_replace(line, "y2", "-g'(x - 2)"))
+        line = str_replace(line, "y1", "f_Cauchy'(x)"),
+        line = str_replace(line, "y2", "-f_Cauchy'(x - 2)"))
 
 df %>% ggplot(aes(x, y, colour = line)) + geom_line()
 ggsave("../assumption6_cauchy.png")
